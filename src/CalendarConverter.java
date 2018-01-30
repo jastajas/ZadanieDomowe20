4ef8c5be1107748aecaf45d89e432b1154354f71
jackS <jastajas@gamil.com>
2018-01-30 20:23:04 +0100

    Klasa CalendarConverter - ver.1 (static method)

---
+++ src/CalendarConverter.java
@@ -0,0 +1,30 @@
+public class CalendarConverter {
+    private static final int MONDAY = 1;
+    private static final int TUESDAY = 2;
+    private static final int WEDNESDAY = 3;
+    private static final int THURSDAY = 4;
+    private static final int FRIDAY = 5;
+    private static final int SATURDAY = 6;
+    private static final int SUNDAY = 7;
+
+    public static String convertDayToString(int dayNumber) {
+        switch (dayNumber) {
+            case MONDAY:
+                return "Poniedziałek";
+            case TUESDAY:
+                return "Wtorek";
+            case WEDNESDAY:
+                return "Środa";
+            case THURSDAY:
+                return "Czwartek";
+            case FRIDAY:
+                return "Piątek";
+            case SATURDAY:
+                return "Sobota";
+            case SUNDAY:
+                return "Niedziela";
+            default:
+                return "Niepoprawna wartość.";
+        }
+    }
+}
